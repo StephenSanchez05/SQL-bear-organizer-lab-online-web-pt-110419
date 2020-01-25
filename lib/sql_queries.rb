64b4834bb24dc9cb5a289db3da9871635ee5f234
Stephen Sanchez <stephensanchez05@gmail.com>
2020-01-25 23:27:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE name = 'NULL';"
 end
